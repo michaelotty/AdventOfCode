bcf6b1a3a9209725cc281b1d00702f9c285c49e6
Michael Otty <michael.otty@gmail.com>
2020-12-29 23:25:13 +0000

    Part 1

--- aoc202006p1.py
+++ aoc202006p1.py
@@ -1,4 +1,14 @@
 """Advent of code 2020 day 6"""
 
-with open('test.txt', 'rt') as f:
-    print(f.read())
+with open('question.txt', 'rt') as f:
+    text = f.read()
+    groups = text.split('\n\n')
+
+    total = 0
+
+    for string in groups:
+        string = string.replace('\n', '')  # Remove whitespace
+        letters = set(string)
+        total += len(letters)
+    
+    print(total)
