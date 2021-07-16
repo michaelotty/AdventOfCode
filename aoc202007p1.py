5fb7e5b92cbe216cfafc1054d1adeedb3a5000f6
Michael Otty <michael.otty@gmail.com>
2021-07-16 17:38:01 +0100

    Tidyup and simplification

--- aoc202007p1.py
+++ aoc202007p1.py
@@ -1,27 +1,30 @@
 """Advent of Code 2020 7 part 1"""
 
 import re
-from queue import Queue
 
-with open('question.txt') as f:
-    file_content = f.read()
+def main():
+    """Main function"""
+    with open('question.txt') as file:
+        file_content = file.read()
 
-search_string_queue = Queue()
-search_string_queue.put('shiny gold bag')
-searched_strings = []
+    search_string_queue = set()
+    search_string_queue.add('shiny gold bag')
+    searched_strings = set()
 
-while True:
-    search_string = search_string_queue.get()
-    pattern = r'\w+ \w+ \w+ contain.*' + search_string + '.*'
-    matches = re.findall(pattern, file_content)
-    for match in matches:
-        match_string = re.findall(r'\w* \w* bag', match)[0]
-        if match_string not in searched_strings:
-            search_string_queue.put(match_string)
-            searched_strings.append(match_string)
+    while True:
+        search_string = search_string_queue.pop()
+        pattern = r'\w+ \w+ \w+ contain.*' + search_string + '.*'
+        matches = re.findall(pattern, file_content)
+        for match in matches:
+            match_string = re.findall(r'\w* \w* bag', match)[0]
+            if match_string not in searched_strings:
+                search_string_queue.add(match_string)
+                searched_strings.add(match_string)
 
-    if search_string_queue.empty():
-        break
+        if len(search_string_queue) == 0:
+            break
 
-print(searched_strings)
-print(len(searched_strings))
+    print(len(searched_strings))
+
+if __name__ == "__main__":
+    main()
