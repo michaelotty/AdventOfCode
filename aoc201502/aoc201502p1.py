69a792e5e4c413b4712762bb08eea6aa5a6d8181
Michael Otty <michael.otty@gmail.com>
2021-09-23 17:38:26 +0100

    Added area_of_cuboid()

--- aoc201502/aoc201502p1.py
+++ aoc201502/aoc201502p1.py
@@ -1,8 +1,14 @@
 """Advent of code Day 2 part 1"""
 
+def area_of_cuboid(length: float, width: float, height: float) -> float:
+    """Finds the surface area of a cuboid"""
+    return 2.0*length*width + 2.0*length*height + 2.0*height*width
+
 
 def main():
     """Main function"""
+    with open('input.txt') as f:
+        print(f.read())
 
 
 if __name__ == "__main__":
