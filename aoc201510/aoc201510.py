de995ef5d26fe547b9cd93631f5bd50dc94e0d64
Michael Otty <michael.otty@gmail.com>
2021-10-03 19:05:16 +0100

    Added solution for 2015 day 10 part 1

---
+++ aoc201510/aoc201510.py
@@ -0,0 +1,33 @@
+"""Advent of code Day 10 part 1 and 2"""
+
+
+def main():
+    """Main function"""
+    with open('input.txt') as f:
+        file_input = f.read()
+
+    output = file_input
+    for _ in range(40):
+        output = find_look_and_say_string(output)
+    print(len(output))
+
+
+def find_look_and_say_string(in_str: str) -> str:
+    """Finds the look-and-say sequence string"""
+    output = str()
+    count = 0
+    last_ch = None
+
+    for ch in in_str:
+        if last_ch != ch and last_ch is not None:
+            output += f"{count}{last_ch}"
+            count = 0
+        count += 1
+        last_ch = ch
+
+    output += f"{count}{last_ch}"
+    return output
+
+
+if __name__ == "__main__":
+    main()
