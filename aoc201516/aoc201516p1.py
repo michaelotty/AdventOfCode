7104dd6fe19273d61e1f2b06e732d8759cb95236
Michael Otty <michael.otty@gmail.com>
2021-10-05 21:58:41 +0100

    Added solution to 2015 day 16 part 1

---
+++ aoc201516/aoc201516p1.py
@@ -0,0 +1,31 @@
+"""Advent of code Day 16 part 1"""
+
+
+def main():
+    """Main function"""
+    with open('input.txt') as file:
+        data = file.readlines()
+
+    matches = ('children: 3', 'cats: 7', 'samoyeds: 2', 'pomeranians: 3', 'akitas: 0',
+               'vizslas: 0', 'goldfish: 5', 'trees: 3', 'cars: 2', 'perfumes: 1')
+
+    for match in matches:
+        data = [line for line in data if match_or_missing(match, line)]
+
+    for line in data:
+        print(line[0:-1])
+
+
+def match_or_missing(match: str, line: str) -> bool:
+    """Returns true if the string matches or if it isn't there at all"""
+    if match.split()[0] in line:
+        if match in line:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    main()
