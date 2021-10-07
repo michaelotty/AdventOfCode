ad061c78e1d72efbd9941bab7d4b8ff81d271599
Michael Otty <michael.otty@gmail.com>
2021-10-07 21:56:00 +0100

    Added solution to 2015 day 17 part 2

---
+++ aoc201517/aoc201517p2.py
@@ -0,0 +1,21 @@
+"""Advent of code Day 17 part 1"""
+
+from itertools import combinations
+
+
+def main():
+    """Main function"""
+    with open('input.txt') as file:
+        data = file.readlines()
+
+    comb = 0
+
+    containers = tuple(int(i) for i in data)
+    for i in combinations(containers, 4):
+        if sum(i) == 150:
+            comb += 1
+    print(comb)
+
+
+if __name__ == "__main__":
+    main()
