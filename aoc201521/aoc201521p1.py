92b012d5ce2f7ec3c98a5749862d5053d006f263
Michael Otty <michael.otty@gmail.com>
2021-10-09 10:18:17 +0100

    Added base solution file for 2015 day 19 to 25

---
+++ aoc201521/aoc201521p1.py
@@ -0,0 +1,13 @@
+"""Advent of code Day 21 part 1"""
+
+
+def main():
+    """Main function"""
+    with open('input.txt') as file:
+        file_contents = file.read()
+
+    print(file_contents)
+
+
+if __name__ == "__main__":
+    main()
