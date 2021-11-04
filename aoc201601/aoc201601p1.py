3b03d24143d9048a2c4b0a62da7931713ca2f450
Michael Otty <michael.otty@gmail.com>
2021-11-04 11:51:47 +0000

    Solution to 2016 day 1 part 1

--- aoc201601/aoc201601p1.py
+++ aoc201601/aoc201601p1.py
@@ -1,12 +1,33 @@
 """Advent of code Day 1 part 1"""
 
+from turtle import forward, left, mode, position, right, setposition, speed
+
 
 def main():
     """Main function"""
-    with open('input.txt') as f:
-        file_contents = f.read().split(', ')
+    with open('input.txt', encoding='utf-8') as f:
+        instructions = f.read().split(', ')
 
-    print(file_contents)
+    setposition((0, 0))
+    mode('logo')
+    speed(0)
+
+    for instruction in instructions:
+        direction = instruction[0]
+        steps = int(instruction[1:])
+
+        if direction == 'R':
+            right(90)
+        elif direction == 'L':
+            left(90)
+        else:
+            raise ValueError
+
+        forward(steps)
+
+    x, y = position()
+    print(round(x + y))
+
 
 if __name__ == "__main__":
     main()
