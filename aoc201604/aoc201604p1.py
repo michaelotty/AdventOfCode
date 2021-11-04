5c3313131cc539d48ce3aca3f8c331acfb3e044a
Michael Otty <michael.otty@gmail.com>
2021-11-04 18:36:45 +0000

    Solution to day 4 part 1

--- aoc201604/aoc201604p1.py
+++ aoc201604/aoc201604p1.py
@@ -11,10 +11,8 @@ def main():
     real_rooms = []
     not_real_rooms = []
 
-    solution = 0
-
     for line in lines:
-        room_name, checksum = line.split(' [')
+        room_name, checksum = line.split('[')
         checksum = checksum.removesuffix(']')
         rearranged_room_name = ''.join(sorted(
             i for i in room_name if i.isalpha()))
@@ -22,16 +20,11 @@ def main():
             rearranged_room_name).most_common())
 
         if new_checksum.startswith(checksum):
-            solution += int(room_name.split('-')[-1])
             real_rooms.append(room_name)
         else:
             not_real_rooms.append(room_name)
 
     print(sum(int(x.split('-')[-1]) for x in real_rooms))
-    print(solution)
-# 503 wrong, too low, amount of rooms not sum of sector IDs
-# 265114 too low
-# 265675 too low
 
 
 if __name__ == "__main__":
