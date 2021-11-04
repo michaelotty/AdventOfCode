ccdfee8ab14cdb0b2e4ffafaf9fd3f06bfb5d0b0
Michael Otty <michael.otty@gmail.com>
2021-11-04 18:24:57 +0000

    Corrected file name

--- aoc201604/aoc201604p1.py
+++ aoc201604/aoc201604p1.py
@@ -11,21 +11,24 @@ def main():
     real_rooms = []
     not_real_rooms = []
 
+    solution = 0
+
     for line in lines:
         room_name, checksum = line.split(' [')
         checksum = checksum.removesuffix(']')
-        rearranged_room_name = sorted(
-            ''.join(i for i in room_name if i.isalpha()))
+        rearranged_room_name = ''.join(sorted(
+            i for i in room_name if i.isalpha()))
         new_checksum = ''.join(x[0] for x in Counter(
-            rearranged_room_name).most_common(5))
+            rearranged_room_name).most_common())
 
         if new_checksum.startswith(checksum):
+            solution += int(room_name.split('-')[-1])
             real_rooms.append(room_name)
         else:
             not_real_rooms.append(room_name)
 
     print(sum(int(x.split('-')[-1]) for x in real_rooms))
-
+    print(solution)
 # 503 wrong, too low, amount of rooms not sum of sector IDs
 # 265114 too low
 # 265675 too low
