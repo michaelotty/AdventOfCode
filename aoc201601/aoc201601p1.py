d217c83567303876ec3317350d70caaad38eda90
Michael Otty <michael.otty@gmail.com>
2021-11-04 11:28:03 +0000

    Added 2016 day 1

---
+++ aoc201601/aoc201601p1.py
@@ -0,0 +1,12 @@
+"""Advent of code Day 1 part 1"""
+
+
+def main():
+    """Main function"""
+    with open('input.txt') as f:
+        file_contents = f.read().split(', ')
+
+    print(file_contents)
+
+if __name__ == "__main__":
+    main()
