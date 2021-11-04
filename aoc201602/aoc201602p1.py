89867a1e7624baa0326b4ccb3bae8a65bcde2e49
Michael Otty <michael.otty@gmail.com>
2021-11-04 12:53:05 +0000

    Solution to day 2 part 1

--- aoc201602/aoc201602p1.py
+++ aoc201602/aoc201602p1.py
@@ -3,10 +3,43 @@
 
 def main():
     """Main function"""
-    with open('input.txt', encoding='utf-8') as f:
-        file_content = f.read()
+    with open('input.txt', encoding='utf-8') as file:
+        digit_sequences = file.read().split()
 
-    print(file_content)
+    # Position of middle digit
+    position = (1, 1)
+    rows = 3
+    cols = 3
+    digits = {(x, y): 1 + x + y*3 for y in range(rows) for x in range(cols)}
+
+    code = []
+
+    for digit_sequence in digit_sequences:
+        for direction in digit_sequence:
+            if direction == 'U':
+                position = (position[0], position[1] - 1)
+            elif direction == 'R':
+                position = (position[0] + 1, position[1])
+            elif direction == 'D':
+                position = (position[0], position[1] + 1)
+            elif direction == 'L':
+                position = (position[0] - 1, position[1])
+            else:
+                raise ValueError(f'{direction} is not a valid direction')
+
+            # Bring back in bounds
+            if position[0] < 0:
+                position = (0, position[1])
+            if position[0] >= cols:
+                position = (cols - 1, position[1])
+            if position[1] < 0:
+                position = (position[0], 0)
+            if position[1] >= rows:
+                position = (position[0], rows - 1)
+
+        code.append(digits[position])
+
+    print(''.join(str(x) for x in code))
 
 
 if __name__ == "__main__":
