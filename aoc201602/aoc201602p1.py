ac5009b98e45f10f1dd2b25a94f1211f2c8f696d
Michael Otty <michael.otty@gmail.com>
2021-11-04 12:16:19 +0000

    Added day 1 puzzle input

---
+++ aoc201602/aoc201602p1.py
@@ -0,0 +1,13 @@
+"""Advent of code Day 2 part 1"""
+
+
+def main():
+    """Main function"""
+    with open('input.txt', encoding='utf-8') as f:
+        file_content = f.read()
+
+    print(file_content)
+
+
+if __name__ == "__main__":
+    main()
