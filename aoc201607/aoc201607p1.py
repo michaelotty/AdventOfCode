dc73cbc39c362b3caaac49b436c5921f096e1c58
Michael Otty <michael.otty@gmail.com>
2021-11-05 21:04:47 +0000

    Day 7 part 1

---
+++ aoc201607/aoc201607p1.py
@@ -0,0 +1,52 @@
+"""Advent of code Day 7 part 1"""
+
+import re
+
+
+def has_abba(test_string: str) -> bool:
+    """An ABBA is any four-character sequence which consists
+    of a pair of two DIFFERENT characters followed by the
+    reverse of that pair, such as xyyx or abba.
+    Not aaaa or tttt."""
+    num_of_substr = len(test_string) - 3
+    for i in range(num_of_substr):
+        if (test_string[i] != test_string[i+1]) and (test_string[i:i+2] == test_string[i+3:i+1:-1]):
+            return True
+    return False
+
+
+def supports_tls(ips: list[str], hypernets: list[str]) -> bool:
+    """An IP supports TLS if it has an Autonomous Bridge
+    Bypass Annotation, or ABBA.
+    However, the IP also must not have an ABBA within any
+    hypernet sequences, which are contained by square
+    brackets."""
+    return any(map(has_abba, ips)) and not any(map(has_abba, hypernets))
+
+
+def main():
+    """Main function"""
+    with open('input.txt', encoding='utf-8') as file:
+        lines = file.read().splitlines()
+
+    ips_that_support_tls = []
+
+    inside_brackets = re.compile(r'\[(\w+)\]')
+    outside_brackets = re.compile(r'\w+\[|\]\w+')
+    word_only = re.compile(r'\w+')
+
+    for line in lines:
+        hypernet_sequences = inside_brackets.findall(line)
+        ip_sequences = outside_brackets.findall(line)
+        ip_sequences = [
+            word_only.search(ip_sequence).group()
+            for ip_sequence in ip_sequences]
+
+        ips_that_support_tls.append(
+            supports_tls(ip_sequences, hypernet_sequences))
+
+    print(sum(ips_that_support_tls))
+
+
+if __name__ == "__main__":
+    main()
