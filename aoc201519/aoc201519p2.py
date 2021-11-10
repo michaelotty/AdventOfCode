d48f015dcfd5a57d1e5428a959cfec0cb4789345
Michael Otty <michael.otty@gmail.com>
2021-11-10 22:11:14 +0000

    Attempt at parallelism

--- aoc201519/aoc201519p2.py
+++ aoc201519/aoc201519p2.py
@@ -1,6 +1,19 @@
 """Advent of code Day 19 part 2"""
 
 import re
+from multiprocessing import Process
+
+distinct_molecules = set()
+
+def processing(replacements: list[tuple[str, str]], formula: str):
+    global distinct_molecules
+    for replace_from, replace_to in replacements:
+        for i in range(len(formula)):
+            left_formula, right_formula = formula[:i], formula[i:]
+            molecule = left_formula + \
+                right_formula.replace(replace_to, replace_from, 1)
+            distinct_molecules.add(molecule)
+    # distinct_molecules.remove(formula)
 
 
 def main():
@@ -12,17 +25,17 @@ def main():
     replacements = replacements.split('\n')
     replacements = [tuple(re.split(r' => ', line)) for line in replacements]
 
-    distinct_molecules = {original_formula}
+    global distinct_molecules
+    distinct_molecules.add(original_formula)
     level = 0
 
     while 'e' not in distinct_molecules:
-        for formula in tuple(distinct_molecules):
-            for replace_from, replace_to in replacements:
-                for i in range(len(formula)):
-                    left_formula, right_formula = formula[:i], formula[i:]
-                    molecule = left_formula + \
-                        right_formula.replace(replace_to, replace_from, 1)
-                    distinct_molecules.add(molecule)
+        processes = [Process(target=processing, args=(replacements, formula)) for formula in tuple(distinct_molecules)]
+        for process in processes:
+            process.start()
+        for process in processes:
+            process.join()
+            
         level += 1
 
     print(distinct_molecules)
