229c613499311e77f39c1b95703a104fe871340c
Michael Otty <michael.otty@gmail.com>
2021-11-19 17:22:00 +0000

    Day 2 part 1

---
+++ aoc201802/aoc201802p1.py
@@ -0,0 +1,31 @@
+"""Advent of code Day 2 part 1"""
+
+from collections import Counter
+
+
+def calculate_checksum(lines: list[str]) -> int:
+    """Calculates the checksum"""
+    count_of_twos = 0
+    count_of_threes = 0
+    lines = [Counter(line).most_common() for line in lines]
+
+    for line in lines:
+        count_of_letters = set(i for _, i in line)
+        if 2 in count_of_letters:
+            count_of_twos += 1
+        if 3 in count_of_letters:
+            count_of_threes += 1
+
+    return count_of_twos * count_of_threes
+
+
+def main():
+    """Main function"""
+    with open('input.txt', encoding='utf-8') as file:
+        lines = file.read().split()
+
+    print(calculate_checksum(lines))
+
+
+if __name__ == "__main__":
+    main()
