1a307c8414a05d86e493f7acbd2bf078cfb3eb48
Michael Otty <michael.otty@gmail.com>
2021-11-25 20:46:52 +0000

    Part 2

--- 02/aoc201902.py
+++ 02/aoc201902.py
@@ -11,27 +11,47 @@ class Opcode(IntEnum):
     END = 99
 
 
-def main():
-    """Main function"""
-    with open('input.txt', encoding='utf-8') as file:
-        numbers = [int(i) for i in file.read().split(',')]
+def init_puzzle_input(file_input: tuple[int], noun: int, verb: int) -> list[int]:
+    """Inits the puzzle input"""
+    numbers = list(file_input)
+    numbers[1] = noun
+    numbers[2] = verb
+    return numbers
 
-    numbers[1] = 12
-    numbers[2] = 2
 
+def solve_puzzle(file_input: tuple[int], noun: int, verb: int) -> int:
+    """Solve the puzzle with corrected input"""
+    numbers = init_puzzle_input(file_input=file_input, noun=noun, verb=verb)
     i = 0
     while True:
-        opcode, *address = numbers[i:i+4]
+        opcode, *parameters = numbers[i:i+4]
         if opcode == Opcode.ADD:
-            numbers[address[2]] = numbers[address[0]] + numbers[address[1]]
+            numbers[parameters[2]] = numbers[parameters[0]] + \
+                numbers[parameters[1]]
         elif opcode == Opcode.MULTIPLY:
-            numbers[address[2]] = numbers[address[0]] * numbers[address[1]]
+            numbers[parameters[2]] = numbers[parameters[0]] * \
+                numbers[parameters[1]]
         elif opcode == Opcode.END:
-            print(f'Part 1: {numbers[0]}')
-            break
+            return numbers[0]
         else:
             raise ValueError(f'Opcode: {opcode} not supported')
         i += 4
+
+
+def main():
+    """Main function"""
+    with open('input.txt', encoding='utf-8') as file:
+        file_input = tuple(int(i) for i in file.read().split(','))
+
+    print(f'Part 1: {solve_puzzle(file_input, 12, 2)}')
+
+    noun = 0
+    while True:
+        for verb in range(noun + 1):
+            if solve_puzzle(file_input, noun, verb) == 19690720:
+                print(f'Part 2: {100 * noun + verb}')
+                return
+        noun += 1
 
 
 if __name__ == "__main__":
