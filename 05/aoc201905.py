5722a1e989e28ba16a6808f5efe8df54d7951e1b
Michael Otty <michael.otty@gmail.com>
2021-11-27 18:02:29 +0000

    Seperate blocks in large if to function calls

--- 05/aoc201905.py
+++ 05/aoc201905.py
@@ -24,6 +24,15 @@ class ParameterMode(IntEnum):
     IMMEDIATE = 1
 
 
+def main():
+    """Main function"""
+    with open('input.txt', encoding='utf-8') as file:
+        file_input = tuple(int(i) for i in file.read().split(','))
+
+    print(f'Part 1: {solve_puzzle(file_input, 1)}')
+    print(f'Part 2: {solve_puzzle(file_input, 5)}')
+
+
 def solve_puzzle(file_input: tuple[int], input_val: int) -> int:
     """Solve the puzzle with corrected input"""
     numbers = list(file_input)
@@ -37,96 +46,32 @@ def solve_puzzle(file_input: tuple[int], input_val: int) -> int:
         address += 1
 
         if opcode == Opcode.ADD:
-            value = 0
-            for parameter_mode in parameter_modes[:2]:
-                if parameter_mode == ParameterMode.POSITION:
-                    value += numbers[numbers[address]]
-                elif parameter_mode == ParameterMode.IMMEDIATE:
-                    value += numbers[address]
-                address += 1
-
-            numbers[numbers[address]] = value
-            address += 1
+            address, numbers = add_op(address, numbers, parameter_modes)
 
         elif opcode == Opcode.MULTIPLY:
-            values = []
-            for parameter_mode in parameter_modes[:2]:
-                if parameter_mode == ParameterMode.POSITION:
-                    values.append(numbers[numbers[address]])
-                elif parameter_mode == ParameterMode.IMMEDIATE:
-                    values.append(numbers[address])
-                address += 1
-
-            numbers[numbers[address]] = values[0] * values[1]
-            address += 1
+            address, numbers = multiply_op(address, numbers, parameter_modes)
 
         elif opcode == Opcode.INPUT:
-            if parameter_modes[0] == ParameterMode.POSITION:
-                numbers[numbers[address]] = input_val
-            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
-                numbers[address] = input_val
-            address += 1
+            address, numbers = input_op(
+                address, numbers, parameter_modes, input_val)
 
         elif opcode == Opcode.OUTPUT:
-            if parameter_modes[0] == ParameterMode.POSITION:
-                return_val = numbers[numbers[address]]
-            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
-                return_val = numbers[address]
-            address += 1
+            address, numbers, return_val = output_op(
+                address, numbers, parameter_modes)
 
         elif opcode == Opcode.JMP_IF_TRUE:
-            if parameter_modes[0] == ParameterMode.POSITION:
-                value = bool(numbers[numbers[address]])
-            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
-                value = bool(numbers[address])
-            address += 1
-
-            if value:
-                if parameter_modes[1] == ParameterMode.POSITION:
-                    address = numbers[numbers[address]]
-                elif parameter_modes[1] == ParameterMode.IMMEDIATE:
-                    address = numbers[address]
-            else:
-                address += 1
+            address, numbers = jmp_if_true_op(
+                address, numbers, parameter_modes)
 
         elif opcode == Opcode.JMP_IF_FALSE:
-            if parameter_modes[0] == ParameterMode.POSITION:
-                value = bool(numbers[numbers[address]])
-            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
-                value = bool(numbers[address])
-            address += 1
-
-            if not value:
-                if parameter_modes[1] == ParameterMode.POSITION:
-                    address = numbers[numbers[address]]
-                elif parameter_modes[1] == ParameterMode.IMMEDIATE:
-                    address = numbers[address]
-            else:
-                address += 1
+            address, numbers = jmp_if_false_op(
+                address, numbers, parameter_modes)
 
         elif opcode == Opcode.LESS_THAN:
-            values = []
-            for parameter_mode in parameter_modes[:2]:
-                if parameter_mode == ParameterMode.POSITION:
-                    values.append(numbers[numbers[address]])
-                elif parameter_mode == ParameterMode.IMMEDIATE:
-                    values.append(numbers[address])
-                address += 1
-
-            numbers[numbers[address]] = int(values[0] < values[1])
-            address += 1
+            address, numbers = less_than_op(address, numbers, parameter_modes)
 
         elif opcode == Opcode.EQUALS:
-            values = []
-            for parameter_mode in parameter_modes[:2]:
-                if parameter_mode == ParameterMode.POSITION:
-                    values.append(numbers[numbers[address]])
-                elif parameter_mode == ParameterMode.IMMEDIATE:
-                    values.append(numbers[address])
-                address += 1
-
-            numbers[numbers[address]] = int(values[0] == values[1])
-            address += 1
+            address, numbers = equals_op(address, numbers, parameter_modes)
 
         elif opcode == Opcode.END:
             return return_val
@@ -135,13 +80,137 @@ def solve_puzzle(file_input: tuple[int], input_val: int) -> int:
             raise ValueError(f'Opcode: {opcode} not supported')
 
 
-def main():
-    """Main function"""
-    with open('input.txt', encoding='utf-8') as file:
-        file_input = tuple(int(i) for i in file.read().split(','))
+def add_op(address: int,
+           numbers: list[int],
+           parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Add operator"""
+    value = 0
+    for parameter_mode in parameter_modes[:2]:
+        if parameter_mode == ParameterMode.POSITION:
+            value += numbers[numbers[address]]
+        elif parameter_mode == ParameterMode.IMMEDIATE:
+            value += numbers[address]
+        address += 1
 
-    print(f'Part 1: {solve_puzzle(file_input, 1)}')
-    print(f'Part 2: {solve_puzzle(file_input, 5)}')
+    numbers[numbers[address]] = value
+    address += 1
+
+    return address, numbers
+
+
+def multiply_op(address: int,
+                numbers: list[int],
+                parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Multiply operator"""
+    values = []
+    for parameter_mode in parameter_modes[:2]:
+        if parameter_mode == ParameterMode.POSITION:
+            values.append(numbers[numbers[address]])
+        elif parameter_mode == ParameterMode.IMMEDIATE:
+            values.append(numbers[address])
+        address += 1
+
+    numbers[numbers[address]] = values[0] * values[1]
+    address += 1
+    return address, numbers
+
+
+def input_op(address: int, numbers: list[int],
+             parameter_modes: list[int],
+             input_val: int) -> tuple[int, list[int]]:
+    """Input operator"""
+    if parameter_modes[0] == ParameterMode.POSITION:
+        numbers[numbers[address]] = input_val
+    elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+        numbers[address] = input_val
+    address += 1
+    return address, numbers
+
+
+def output_op(address: int,
+              numbers: list[int],
+              parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Output operator"""
+    if parameter_modes[0] == ParameterMode.POSITION:
+        return_val = numbers[numbers[address]]
+    elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+        return_val = numbers[address]
+    address += 1
+    return address, numbers, return_val
+
+
+def jmp_if_true_op(address: int,
+                   numbers: list[int],
+                   parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Jump if true operator"""
+    if parameter_modes[0] == ParameterMode.POSITION:
+        value = bool(numbers[numbers[address]])
+    elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+        value = bool(numbers[address])
+    address += 1
+
+    if value:
+        if parameter_modes[1] == ParameterMode.POSITION:
+            address = numbers[numbers[address]]
+        elif parameter_modes[1] == ParameterMode.IMMEDIATE:
+            address = numbers[address]
+    else:
+        address += 1
+    return address, numbers
+
+
+def jmp_if_false_op(address: int,
+                       numbers: list[int],
+                       parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Jump if false operator"""
+    if parameter_modes[0] == ParameterMode.POSITION:
+        value = bool(numbers[numbers[address]])
+    elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+        value = bool(numbers[address])
+    address += 1
+
+    if not value:
+        if parameter_modes[1] == ParameterMode.POSITION:
+            address = numbers[numbers[address]]
+        elif parameter_modes[1] == ParameterMode.IMMEDIATE:
+            address = numbers[address]
+    else:
+        address += 1
+    return address, numbers
+
+
+def less_than_op(address: int,
+                 numbers: list[int],
+                 parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Less than operator"""
+    values = []
+    for parameter_mode in parameter_modes[:2]:
+        if parameter_mode == ParameterMode.POSITION:
+            values.append(numbers[numbers[address]])
+        elif parameter_mode == ParameterMode.IMMEDIATE:
+            values.append(numbers[address])
+        address += 1
+
+    numbers[numbers[address]] = int(values[0] < values[1])
+    address += 1
+    return address, numbers
+
+
+def equals_op(address: int,
+              numbers: list[int],
+              parameter_modes: list[int]) -> tuple[int, list[int]]:
+    """Equals operator"""
+    values = []
+    for parameter_mode in parameter_modes[:2]:
+        if parameter_mode == ParameterMode.POSITION:
+            values.append(numbers[numbers[address]])
+        elif parameter_mode == ParameterMode.IMMEDIATE:
+            values.append(numbers[address])
+        address += 1
+
+    numbers[numbers[address]] = int(values[0] == values[1])
+    address += 1
+    return address, numbers
 
 
 if __name__ == "__main__":
