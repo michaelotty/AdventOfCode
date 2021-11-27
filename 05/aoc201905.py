89b6fb19b928d9e5f0074f02b6c226c49d881deb
Michael Otty <michael.otty@gmail.com>
2021-11-27 00:44:11 +0000

    Part 1

--- 05/aoc201905.py
+++ 05/aoc201905.py
@@ -13,35 +13,71 @@ class Opcode(IntEnum):
     END = 99
 
 
-def init_puzzle_input(file_input: tuple[int], noun: int, verb: int) -> list[int]:
-    """Inits the puzzle input"""
-    numbers = list(file_input)
-    numbers[1] = noun
-    numbers[2] = verb
-    return numbers
+@unique
+class ParameterMode(IntEnum):
+    """Parameter modes"""
+    POSITION = 0
+    IMMEDIATE = 1
 
 
-def solve_puzzle(file_input: tuple[int], noun: int, verb: int) -> int:
+def solve_puzzle(file_input: tuple[int], input_val: int) -> int:
     """Solve the puzzle with corrected input"""
-    numbers = init_puzzle_input(file_input=file_input, noun=noun, verb=verb)
-    i = 0
+    numbers = list(file_input)
+    address = 0
+    return_val = None
+
     while True:
-        opcode, *parameters = numbers[i:i+4]
+        opcode = f'{numbers[address]:05d}'
+        parameter_modes = [int(i) for i in opcode[2::-1]]
+        opcode = int(opcode[-2:])
+        address += 1
+
         if opcode == Opcode.ADD:
-            numbers[parameters[2]] = numbers[parameters[0]] + \
-                numbers[parameters[1]]
+            value = 0
+            for parameter_mode in parameter_modes[:2]:
+                if parameter_mode == ParameterMode.POSITION:
+                    value += numbers[numbers[address]]
+                elif parameter_mode == ParameterMode.IMMEDIATE:
+                    value += numbers[address]
+                address += 1
+
+            numbers[numbers[address]] = value
+            address += 1
+
         elif opcode == Opcode.MULTIPLY:
-            numbers[parameters[2]] = numbers[parameters[0]] * \
-                numbers[parameters[1]]
+            values = []
+            for parameter_mode in parameter_modes[:2]:
+                if parameter_mode == ParameterMode.POSITION:
+                    values.append(numbers[numbers[address]])
+                elif parameter_mode == ParameterMode.IMMEDIATE:
+                    values.append(numbers[address])
+                address += 1
+
+            numbers[numbers[address]] = values[0] * values[1]
+            address += 1
+
         elif opcode == Opcode.END:
-            return numbers[0]
+            return return_val
+
         elif opcode == Opcode.INPUT:
-            pass
+            if parameter_modes[0] == ParameterMode.POSITION:
+                numbers[numbers[address]] = input_val
+            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+                numbers[address] = input_val
+            address += 1
+
         elif opcode == Opcode.OUTPUT:
-            pass
+            if parameter_modes[0] == ParameterMode.POSITION:
+                value = numbers[numbers[address]]
+            elif parameter_modes[0] == ParameterMode.IMMEDIATE:
+                value = numbers[address]
+
+            if value != 0:
+                return_val = value
+            address += 1
+
         else:
             raise ValueError(f'Opcode: {opcode} not supported')
-        i += 4
 
 
 def main():
@@ -49,15 +85,7 @@ def main():
     with open('input.txt', encoding='utf-8') as file:
         file_input = tuple(int(i) for i in file.read().split(','))
 
-    print(f'Part 1: {solve_puzzle(file_input, 12, 2)}')
-
-    noun = 0
-    while True:
-        for verb in range(noun + 1):
-            if solve_puzzle(file_input, noun, verb) == 19690720:
-                print(f'Part 2: {100 * noun + verb}')
-                return
-        noun += 1
+    print(f'Part 1: {solve_puzzle(file_input, 1)}')
 
 
 if __name__ == "__main__":
