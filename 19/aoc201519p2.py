b2627e4f7b815d4946a03571c9b15bb8c428ccf2
Michael Otty <michael.otty@gmail.com>
2021-11-28 21:43:07 +0000

    Solution helped by reddit

--- 19/aoc201519p2.py
+++ 19/aoc201519p2.py
@@ -1,41 +1,53 @@
 """Advent of code Day 19 part 2"""
 
 import re
-from multiprocessing import Process
+from random import shuffle
 
-def processing(replacements: list[tuple[str, str]], formula: str, list_of_formula: list):
-    for replace_from, replace_to in replacements:
-        for i in range(len(formula)):
-            left_formula, right_formula = formula[:i], formula[i:]
-            molecule = left_formula + \
-                right_formula.replace(replace_to, replace_from, 1)
-            list_of_formula.append(molecule)
+
+def gen_sequence(X, replacements):
+    """Ngl, stole this from reddit"""
+    for j, i in replacements:
+        for k in range(len(X)):
+            if X[k:k+len(i)] == i:
+                y = X[:k] + j + X[k+len(i):]
+                yield y
 
 
 def main():
     """Main function"""
     with open('input.txt', encoding='utf-8') as file:
-        file_contents = file.read()
+        lines = file.read().splitlines()
 
-    replacements, original_formula = file_contents.split('\n\n')
-    replacements = replacements.split('\n')
-    replacements = [tuple(re.split(r' => ', line)) for line in replacements]
+    replacements = []
+    for i in lines[:-2]:
+        molecule = re.findall(r'(\S+) => (\S+)', i)
+        replacements.append(molecule[0])
+    medicine_molecule = lines[-1]
+    while True:
+        shuffle(replacements)
 
-    distinct_molecules = []
-    distinct_molecules.append(original_formula)
-    level = 0
+        visited = {medicine_molecule}
+        molecule = [medicine_molecule]
 
-    while 'e' not in distinct_molecules:
-        processes = [Process(target=processing, args=(replacements, formula, distinct_molecules)) for formula in tuple(distinct_molecules)]
-        for process in processes:
-            process.start()
-        for process in processes:
-            process.join()
-            
-        level += 1
-
-    print(distinct_molecules)
-    print(level)
+        count = 0
+        while True:
+            made_molecule = []
+            for i in molecule:
+                for j in gen_sequence(i, replacements):
+                    if j in visited:
+                        continue
+                    made_molecule.append(j)
+                    visited.add(j)
+                    break
+            molecule = made_molecule
+            count += 1
+            if molecule or made_molecule:
+                reduced_len = min(map(len, molecule))
+            else:
+                break
+        if reduced_len == 1:
+            print(count)
+            break
 
 
 if __name__ == "__main__":
