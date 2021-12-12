d3afd4ea106aee5b4b258e5986808c807bbd24e2
Michael Otty <michael.otty@gmail.com>
2021-12-12 15:24:47 +0000

    Refactor

--- aoc202004p1.py
+++ aoc202004p1.py
@@ -1,14 +1,25 @@
 """Advent of code 2020 day 4"""
 
-with open('question.txt', 'rt') as f:
-    text = f.read()
-    lines = text.split('\n\n')
+import re
+
+def part_1(lines):
+    """Part 1 of puzzle"""
     valid_records = 0
 
     for line in lines:
-        line = line.split()
-        record = {'ecl': None, 'pid': None, 'eyr': None, 'hcl': None,
-                  'byr': None, 'iyr': None, 'cid': None, 'hgt': None}
+        record = dict(re.findall(r'(\w+):(\w+)', line))
+
+        numeric_keys = ['byr', 'iyr', 'eyr', 'pid', 'cid']
+        for numeric_key in numeric_keys:
+            if 
+            record.get(numeric_key)
+        record = dict(i.split(':') for i in line)
+        print(record)
+
+
+
+        # record = {'ecl': None, 'pid': None, 'eyr': None, 'hcl': None,
+        #           'byr': None, 'iyr': None, 'cid': None, 'hgt': None}
         for item in line:
             item = item.split(':')
             key = item[0]
@@ -21,4 +32,22 @@ with open('question.txt', 'rt') as f:
         else:
             valid_records += 1
 
-    print(valid_records)
+    return valid_records
+
+
+def part_2(lines):
+    """Part 2 of puzzle"""
+    return lines
+
+
+def main():
+    """Start of program execution"""
+    with open('test.txt', encoding='utf-8') as file:
+        lines = file.read().split('\n\n')
+
+    print(f'Part 1: {part_1(lines)}')
+    print(f'Part 2: {part_2(lines)}')
+
+
+if __name__ == "__main__":
+    main()
