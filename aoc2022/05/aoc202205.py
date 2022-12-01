ba99e5645e035447f2b93d754aba5b491d2fdb7e
Michael Otty <michael.otty@gmail.com>
2022-12-01 19:00:53 +0000

    Add blank templates for all 2022 days

---
+++ aoc2022/05/aoc202205.py
@@ -0,0 +1,24 @@
+"""Advent of code Day 5."""
+
+
+def main():
+    """Main function."""
+    with open("aoc2022/05/input.txt", encoding="utf-8") as file:
+        text = file.read()
+    print(text)
+    print(f"Part 1: {part_1()}")
+    print(f"Part 2: {part_2()}")
+
+
+def part_1():
+    """Solve part 1."""
+    return 0
+
+
+def part_2():
+    """Solve part 1."""
+    return 0
+
+
+if __name__ == "__main__":
+    main()
