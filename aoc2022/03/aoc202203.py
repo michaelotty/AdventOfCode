6ebdeb86f5176d7e70086bb75edfd917955bdf28
Michael Otty <michael.otty@gmail.com>
2022-12-03 16:17:15 +0000

    Complete 2022 day 3 part 1

--- aoc2022/03/aoc202203.py
+++ aoc2022/03/aoc202203.py
@@ -1,22 +1,30 @@
 """Advent of code Day 3."""
 
+import string
+
 
 def main():
     """Main function."""
     with open("aoc2022/03/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+        text = file.read().split()
+    print(f"Part 1: {part_1(text)}")
+    print(f"Part 2: {part_2(text)}")
 
 
-def part_1():
+def part_1(puzzle):
     """Solve part 1."""
-    return 0
+    scores = {letter: i for i, letter in enumerate(string.ascii_letters, start=1)}
+
+    counts = sum(
+        scores[(set(item[: (len(item) // 2)]) & set(item[(len(item) // 2) :])).pop()]
+        for item in puzzle
+    )
+
+    return counts
 
 
-def part_2():
-    """Solve part 1."""
+def part_2(puzzle):
+    """Solve part 2."""
     return 0
 
 
