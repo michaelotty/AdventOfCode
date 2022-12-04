7fa9bd504a5cacd0473b23593d4fc5fdd056fbac
Michael Otty <michael.otty@gmail.com>
2022-12-04 12:24:20 +0000

    Solve 2022 day 3 part 2

--- aoc2022/03/aoc202203.py
+++ aoc2022/03/aoc202203.py
@@ -6,26 +6,32 @@ import string
 def main():
     """Main function."""
     with open("aoc2022/03/input.txt", encoding="utf-8") as file:
-        text = file.read().split()
-    print(f"Part 1: {part_1(text)}")
-    print(f"Part 2: {part_2(text)}")
+        rucksacks = file.read().split()
+    priorities = {letter: i for i, letter in enumerate(string.ascii_letters, start=1)}
+
+    print(f"Part 1: {part_1(rucksacks, priorities)}")
+    print(f"Part 2: {part_2(rucksacks, priorities)}")
 
 
-def part_1(puzzle):
+def part_1(rucksacks, priorities):
     """Solve part 1."""
-    scores = {letter: i for i, letter in enumerate(string.ascii_letters, start=1)}
-
-    counts = sum(
-        scores[(set(item[: (len(item) // 2)]) & set(item[(len(item) // 2) :])).pop()]
-        for item in puzzle
+    return sum(
+        priorities[
+            (
+                set(rucksack[: (len(rucksack) // 2)])
+                & set(rucksack[(len(rucksack) // 2) :])
+            ).pop()
+        ]
+        for rucksack in rucksacks
     )
 
-    return counts
 
-
-def part_2(puzzle):
+def part_2(rucksacks, priorities):
     """Solve part 2."""
-    return 0
+    return sum(
+        priorities[(set(elf1) & set(elf2) & set(elf3)).pop()]
+        for elf1, elf2, elf3 in zip(rucksacks[::3], rucksacks[1::3], rucksacks[2::3])
+    )
 
 
 if __name__ == "__main__":
