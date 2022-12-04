582fd296dc22480eaccfd38b7657e447ad4c144f
Michael Otty <michael.otty@gmail.com>
2022-12-04 12:41:55 +0000

    Solve 2022 day 4 part 1

--- aoc2022/04/aoc202204.py
+++ aoc2022/04/aoc202204.py
@@ -4,18 +4,27 @@
 def main():
     """Main function."""
     with open("aoc2022/04/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+        expressions = [
+            [tuple(int(num) for num in nums.split("-")) for nums in line.split(",")]
+            for line in file.read().split()
+        ]
+    print(f"Part 1: {part_1(expressions)}")
+    print(f"Part 2: {part_2(expressions)}")
 
 
-def part_1():
+def part_1(expressions):
     """Solve part 1."""
-    return 0
+    count = 0
+    for expr1, expr2 in expressions:
+        left_range = set(range(expr1[0], expr1[1] + 1))
+        right_range = set(range(expr2[0], expr2[1] + 1))
+        if len(left_range & right_range) in (len(left_range), len(right_range)):
+            count += 1
+
+    return count
 
 
-def part_2():
+def part_2(expressions):
     """Solve part 2."""
     return 0
 
