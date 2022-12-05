25164f0df54f507907b6648afebc664abce380b9
Michael Otty <michael.otty@gmail.com>
2022-12-05 19:00:38 +0000

    Solve 2022 day 5 part 1

--- aoc2022/05/aoc202205.py
+++ aoc2022/05/aoc202205.py
@@ -1,21 +1,38 @@
 """Advent of code Day 5."""
 
+import re
+
 
 def main():
     """Main function."""
     with open("aoc2022/05/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+        stacks, instructions = file.read().split("\n\n")
+        stacks = stacks.split("\n")
+        instructions = instructions.split("\n")
+
+    # Extract stack data from file into useful
+    indexes = range(1, len(stacks[0]), 4)
+    stacks = stacks[:-1]
+    stacks = [[line[i] for line in reversed(stacks) if line[i] != " "] for i in indexes]
+
+    # Simplify instructions
+    instructions = [
+        tuple(int(i) for i in re.findall(r"\d+", line)) for line in instructions
+    ]
+
+    print(f"Part 1: {part_1(stacks, instructions)}")
+    print(f"Part 2: {part_2(stacks, instructions)}")
 
 
-def part_1():
+def part_1(stacks: list[list[str]], instructions):
     """Solve part 1."""
-    return 0
+    for amount, from_stack, to_stack in instructions:
+        for _ in range(amount):
+            stacks[to_stack - 1].append(stacks[from_stack - 1].pop())
+    return "".join(stack[-1] for stack in stacks)
 
 
-def part_2():
+def part_2(stacks, instructions):
     """Solve part 2."""
     return 0
 
