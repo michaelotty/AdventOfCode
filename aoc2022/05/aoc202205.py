f6067186992095e7bf2d8cb8dbd178a9be4bc51b
Michael Otty <michael.otty@gmail.com>
2022-12-05 19:17:22 +0000

    Solve 2022 day 5 part 2

--- aoc2022/05/aoc202205.py
+++ aoc2022/05/aoc202205.py
@@ -1,5 +1,6 @@
 """Advent of code Day 5."""
 
+import copy
 import re
 
 
@@ -10,31 +11,38 @@ def main():
         stacks = stacks.split("\n")
         instructions = instructions.split("\n")
 
-    # Extract stack data from file into useful
+    # Structure stacks as list of lists
     indexes = range(1, len(stacks[0]), 4)
-    stacks = stacks[:-1]
+    stacks = stacks[:-1]  # Remove the line with the stack IDs
     stacks = [[line[i] for line in reversed(stacks) if line[i] != " "] for i in indexes]
 
-    # Simplify instructions
+    # Convert "move a from b to c" to (a, b, c)
     instructions = [
-        tuple(int(i) for i in re.findall(r"\d+", line)) for line in instructions
+        tuple(int(num) for num in re.findall(r"\d+", line)) for line in instructions
     ]
 
-    print(f"Part 1: {part_1(stacks, instructions)}")
+    # Copy so we can use again for part 2
+    print(f"Part 1: {part_1(copy.deepcopy(stacks), instructions)}")
     print(f"Part 2: {part_2(stacks, instructions)}")
 
 
-def part_1(stacks: list[list[str]], instructions):
+def part_1(stacks: list[list[str]], instructions: list[tuple[int, int, int]]):
     """Solve part 1."""
     for amount, from_stack, to_stack in instructions:
         for _ in range(amount):
             stacks[to_stack - 1].append(stacks[from_stack - 1].pop())
+
     return "".join(stack[-1] for stack in stacks)
 
 
-def part_2(stacks, instructions):
+def part_2(stacks: list[list[str]], instructions: list[tuple[int, int, int]]):
     """Solve part 2."""
-    return 0
+    for amount, from_stack, to_stack in instructions:
+        stacks[to_stack - 1].extend(stacks[from_stack - 1][-amount:])
+        for _ in range(amount):
+            stacks[from_stack - 1].pop()
+
+    return "".join(stack[-1] for stack in stacks)
 
 
 if __name__ == "__main__":
