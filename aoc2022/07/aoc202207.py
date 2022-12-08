d0a5d837011780e4b787d9b5df778db892756754
Michael Otty <michael.otty@gmail.com>
2022-12-08 23:28:35 +0000

    Solve 2022 day 7

--- aoc2022/07/aoc202207.py
+++ aoc2022/07/aoc202207.py
@@ -1,71 +1,83 @@
 """Advent of code Day 7."""
 
-import re
-
 
 def main():
     """Main function."""
-    with open("aoc2022/07/test.txt", encoding="utf-8") as file:
-        text = file.read().splitlines()
-    tree = build_tree(text)
+    with open("aoc2022/07/input.txt", encoding="utf-8") as file:
+        text = file.read().splitlines()[1:]
+    tree = {}
+    text, tree = build_tree(text, tree)
     print("Part 1:", part_1(tree))
     print("Part 2:", part_2(tree))
 
 
-def build_tree(text):
+def build_tree(text: list[str], tree: dict) -> tuple[list[str], dict]:
     """Build the tree."""
-    cd = ""
-    fs = {}
-    for line in text:
+    while text:
+        line = text.pop(0)
         first, second, *third = line.split()
+
         if first == "$":
             if second == "cd":
                 if third[0] == "..":
-                    left = re.findall(r"(.*)\/\w+", cd)[0]
-                    cd = left
-                elif third[0] == "/":
-                    cd += ""
-                else:
-                    cd += "/" + third[0]
+                    return text, tree
+                text, tree[third[0]] = build_tree(text, tree[third[0]])
 
             if second == "ls":
                 pass
 
         elif first == "dir":
-            pass
+            tree[second] = {}
         else:
-            fs[cd + "/" + second] = int(first)
+            tree[second] = int(first)
 
-    return fs
+    return text, tree
 
 
-def part_1(tree):
+def part_1(tree: dict):
     """Solve part 1."""
+    size = 0
+    for val in tree.values():
+        if isinstance(val, dict):
+            dir_size = get_directory_size(val)
+            if dir_size <= 100000:
+                size += dir_size
+            size += part_1(val)
+
+    return size
 
 
-def part_2(tree):
+def part_2(tree: dict) -> int:
     """Solve part 2."""
-    return 0
+    fs_size = 70000000
+    update_size = 30000000
+    free_space = fs_size - get_directory_size(tree)
+    space_needed = update_size - free_space
+
+    return find_best_directory_to_delete(tree, space_needed, fs_size)
 
 
-"""
-- / (dir)
-  - a (dir)
-    - e (dir)
-      - i (file, size=584)
-    - f (file, size=29116)
-    - g (file, size=2557)
-    - h.lst (file, size=62596)
-  - b.txt (file, size=14848514)
-  - c.dat (file, size=8504156)
-  - d (dir)
-    - j (file, size=4060174)
-    - d.log (file, size=8033020)
-    - d.ext (file, size=5626152)
-    - k (file, size=7214296)
-"""
+def find_best_directory_to_delete(tree: dict, space_needed: int, size: int) -> int:
+    """Finds the smallest directory, which is at least space_needed size."""
+    for val in tree.values():
+        if isinstance(val, dict):
+            dir_size = get_directory_size(val)
+            if space_needed <= dir_size < size:
+                size = dir_size
+            size = find_best_directory_to_delete(val, space_needed, size)
 
-# {"/": {"a": {}, "b.txt": 14848514, "c.dat": 8504156, "d": {}}}
+    return size
+
+
+def get_directory_size(tree: dict) -> int:
+    """Get the total size for a directory."""
+    size = 0
+    for val in tree.values():
+        if isinstance(val, dict):
+            size += get_directory_size(val)
+        else:
+            size += val
+    return size
 
 
 if __name__ == "__main__":
--- aoc2022/08/aoc202208.py
+++ aoc2022/08/aoc202208.py
@@ -3,19 +3,19 @@
 
 def main():
     """Main function."""
-    with open("aoc2022/08/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    with open("aoc2022/08/test.txt", encoding="utf-8") as file:
+        number_grid = [[int(num) for num in line] for line in file.read().splitlines()]
+    print(number_grid)
+    print("Part 1:", part_1(number_grid))
+    print("Part 2:", part_2(number_grid))
 
 
-def part_1():
+def part_1(number_grid):
     """Solve part 1."""
     return 0
 
 
-def part_2():
+def part_2(number_grid):
     """Solve part 2."""
     return 0
 
