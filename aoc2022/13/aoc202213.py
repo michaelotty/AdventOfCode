024eb7e439a361b0e10b1f2bfa42e64bca88ce15
Michael Otty <michael.otty@gmail.com>
2022-12-13 07:55:26 +0000

    Add input and test for 2022 day 13

--- aoc2022/13/aoc202213.py
+++ aoc2022/13/aoc202213.py
@@ -4,18 +4,19 @@
 def main():
     """Main function."""
     with open("aoc2022/13/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+        packet_pairs = [pair.split() for pair in file.read().split("\n\n")]
+
+    print(packet_pairs)
+    print("Part 1:", part_1(packet_pairs))
+    print("Part 2:", part_2(packet_pairs))
 
 
-def part_1():
+def part_1(packet_pairs):
     """Solve part 1."""
     return 0
 
 
-def part_2():
+def part_2(packet_pairs):
     """Solve part 2."""
     return 0
 
