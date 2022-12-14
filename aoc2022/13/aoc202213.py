dfd2394d69fa9e65890c277d03e95b1876947081
Michael Otty <michael.otty@gmail.com>
2022-12-14 00:11:50 +0000

    Solve 2022 day 13 part 2 but answer too high

--- aoc2022/13/aoc202213.py
+++ aoc2022/13/aoc202213.py
@@ -1,5 +1,7 @@
 """Advent of code Day 13."""
 
+from __future__ import annotations
+
 
 def main():
     """Main function."""
@@ -53,7 +55,41 @@ def left_is_less(left, right):
 
 def part_2(packet_pairs):
     """Solve part 2."""
+    new_packets = [Packet([[2]]), Packet([[6]])]
+    packets = [Packet(packet) for pair in packet_pairs for packet in pair]
+    packets += new_packets
+    packets.sort()
+
+    nums = []
+    for i, packet in enumerate(packets, start=1):
+        if packet in new_packets:
+            nums.append(i)
+    if len(nums) == 2:
+        return nums[0] * nums[1]
     return 0
+
+
+# 22866 too high
+
+
+class Packet:
+    """Packet comparison class."""
+
+    def __init__(self, packet):
+        """Create the Packet class."""
+        self.packet = packet
+
+    def __lt__(self, other: Packet):
+        """Find if Packet is less than other packet."""
+        return left_is_less(self.packet, other.packet)
+
+    def __eq__(self, other: Packet):
+        """Find if Packet is equal to other packet."""
+        return str(self.packet) == str(other.packet)
+
+    def __repr__(self) -> str:
+        """String representation of packet."""
+        return str(self.packet)
 
 
 if __name__ == "__main__":
