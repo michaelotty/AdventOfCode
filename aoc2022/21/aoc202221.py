4ef62427819b1dcb17c84442adb6024022508061
Michael Otty <michael.otty@gmail.com>
2022-12-21 17:53:23 +1100

    Solve 2022 day 21 part 1

--- aoc2022/21/aoc202221.py
+++ aoc2022/21/aoc202221.py
@@ -4,15 +4,35 @@
 def main():
     """Main function."""
     with open("aoc2022/21/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
+        monkeys = dict(line.split(": ") for line in file.read().splitlines())
+    print("Part 1:", part_1(monkeys))
     print("Part 2:", part_2())
 
 
-def part_1():
+def part_1(monkeys: dict[str, str]) -> int:
     """Solve part 1."""
-    return 0
+    return find_value("root", monkeys)
+
+
+def find_value(key: str, monkeys: dict[str, str]):
+    """Find the value of a monkey given by key."""
+    val = monkeys[key]
+    try:
+        return int(val)
+    except ValueError:
+        pass
+
+    left, op, right = val.split()
+
+    match op:
+        case "+":
+            return find_value(left, monkeys) + find_value(right, monkeys)
+        case "-":
+            return find_value(left, monkeys) - find_value(right, monkeys)
+        case "*":
+            return find_value(left, monkeys) * find_value(right, monkeys)
+        case "/":
+            return find_value(left, monkeys) // find_value(right, monkeys)
 
 
 def part_2():
