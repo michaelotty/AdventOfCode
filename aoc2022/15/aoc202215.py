372f358cf71849e564f7a925b3a5a8fbcf1f0e85
Michael Otty <michael.otty@gmail.com>
2022-12-22 09:47:08 +1100

    Nearly working solution to 2022 day 15 p1

--- aoc2022/15/aoc202215.py
+++ aoc2022/15/aoc202215.py
@@ -11,17 +11,22 @@ def main():
         coords = [map(int, nums) for nums in re.findall(regex, file.read())]
         coords = {(x1, y1): (x2, y2) for x1, y1, x2, y2 in coords}
 
-    print("Part 1:", part_1(coords))
+    print("Part 1:", part_1(coords, 10))
     print("Part 2:", part_2(coords))
 
 
-def part_1(coords: dict[tuple[int, int], tuple[int, int]]) -> int:
+def part_1(coords: dict[tuple[int, int], tuple[int, int]], y_to_check: int) -> int:
     """Solve part 1."""
-    sensor_no_go_zone = set()
+    beacon_no_go_zone = set()
 
     for sensor, beacon in coords.items():
-        print(manhattan_distance(sensor, beacon))
-    return 0
+        distance = manhattan_distance(sensor, beacon)
+        for x in range(-distance, distance + 1):
+            pos = (x, y_to_check)
+            if manhattan_distance(pos, sensor) <= distance:
+                beacon_no_go_zone.add(pos)
+
+    return beacon_no_go_zone
 
 
 def manhattan_distance(start: tuple[int, int], end: tuple[int, int]) -> int:
