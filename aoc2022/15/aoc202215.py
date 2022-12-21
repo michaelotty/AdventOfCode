f0af1c0c16e97770b1ddf633b442283be3a61b48
Michael Otty <michael.otty@gmail.com>
2022-12-22 09:37:54 +1100

    Begin work on 2022 day 15

--- aoc2022/15/aoc202215.py
+++ aoc2022/15/aoc202215.py
@@ -1,21 +1,35 @@
 """Advent of code Day 15."""
 
+import re
+
 
 def main():
     """Main function."""
-    with open("aoc2022/15/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    regex = r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)"
+
+    with open("aoc2022/15/test1.txt", encoding="utf-8") as file:
+        coords = [map(int, nums) for nums in re.findall(regex, file.read())]
+        coords = {(x1, y1): (x2, y2) for x1, y1, x2, y2 in coords}
+
+    print("Part 1:", part_1(coords))
+    print("Part 2:", part_2(coords))
 
 
-def part_1():
+def part_1(coords: dict[tuple[int, int], tuple[int, int]]) -> int:
     """Solve part 1."""
+    sensor_no_go_zone = set()
+
+    for sensor, beacon in coords.items():
+        print(manhattan_distance(sensor, beacon))
     return 0
 
 
-def part_2():
+def manhattan_distance(start: tuple[int, int], end: tuple[int, int]) -> int:
+    """Calculate the manhattan distance between two points."""
+    return abs(end[0] - start[0]) + abs(end[1] - start[1])
+
+
+def part_2(coords: dict[tuple[int, int], tuple[int, int]]) -> int:
     """Solve part 2."""
     return 0
 
