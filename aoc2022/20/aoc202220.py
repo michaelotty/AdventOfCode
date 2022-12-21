fc5d5abca932a5363327eccf1fbe052982c6fd37
Michael Otty <michael.otty@gmail.com>
2022-12-21 14:20:31 +1100

    Solve 2022 day 20

--- aoc2022/20/aoc202220.py
+++ aoc2022/20/aoc202220.py
@@ -4,20 +4,50 @@
 def main():
     """Main function."""
     with open("aoc2022/20/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+        nums = list(map(int, file.read().split()))
+    print("Part 1:", part_1(nums))
+    print("Part 2:", part_2(nums))
 
 
-def part_1():
+def part_1(nums: list[int]):
     """Solve part 1."""
-    return 0
+    indexes = list(range(len(nums)))
+    for i in indexes.copy():
+        from_index = indexes.index(i)
+        indexes.pop(from_index)
+        to_index = (from_index + nums[i] - 1) % len(indexes) + 1
+        indexes.insert(to_index, i)
+    mixed = [nums[i] for i in indexes]
+    zero_index = mixed.index(0)
+    return sum(
+        (
+            mixed[(1000 + zero_index) % len(mixed)],
+            mixed[(2000 + zero_index) % len(mixed)],
+            mixed[(3000 + zero_index) % len(mixed)],
+        )
+    )
 
 
-def part_2():
+def part_2(nums: list[int]):
     """Solve part 2."""
-    return 0
+    decrypt_key = 811589153
+    nums = [num * decrypt_key for num in nums]
+
+    indexes = list(range(len(nums)))
+    for i in indexes.copy() * 10:
+        from_index = indexes.index(i)
+        indexes.pop(from_index)
+        to_index = (from_index + nums[i] - 1) % len(indexes) + 1
+        indexes.insert(to_index, i)
+    mixed = [nums[i] for i in indexes]
+    zero_index = mixed.index(0)
+    return sum(
+        (
+            mixed[(1000 + zero_index) % len(mixed)],
+            mixed[(2000 + zero_index) % len(mixed)],
+            mixed[(3000 + zero_index) % len(mixed)],
+        )
+    )
 
 
 if __name__ == "__main__":
