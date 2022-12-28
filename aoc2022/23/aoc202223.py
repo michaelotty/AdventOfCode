79696b4eb030c12a397aa24f509cfd0714a1a097
Michael Otty <michael.otty@gmail.com>
2022-12-29 00:28:21 +1100

    Add test input to 2022 day 23

--- aoc2022/23/aoc202223.py
+++ aoc2022/23/aoc202223.py
@@ -3,19 +3,21 @@
 
 def main():
     """Main function."""
-    with open("aoc2022/23/input.txt", encoding="utf-8") as file:
-        text = file.read()
-    print(text)
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    with open("aoc2022/23/test1.txt", encoding="utf-8") as file:
+        grid = [
+            [1 if char == "#" else 0 for char in line]
+            for line in file.read().splitlines()
+        ]
+    print("Part 1:", part_1(grid), sep="\n")
+    print("Part 2:", part_2(grid))
 
 
-def part_1():
+def part_1(grid):
     """Solve part 1."""
-    return 0
+    return "\n".join(str(line) for line in grid)
 
 
-def part_2():
+def part_2(grid):
     """Solve part 2."""
     return 0
 
