faae6e87283fb3bcfd0ad3c118dc7c678d6fca2b
Michael Otty <michael.otty@gmail.com>
2022-12-28 11:32:49 +1100

    Solve 2022 day 15 part 1

--- aoc2022/15/aoc202215.py
+++ aoc2022/15/aoc202215.py
@@ -7,11 +7,11 @@ def main():
     """Main function."""
     regex = r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)"
 
-    with open("aoc2022/15/test1.txt", encoding="utf-8") as file:
+    with open("aoc2022/15/input.txt", encoding="utf-8") as file:
         coords = [map(int, nums) for nums in re.findall(regex, file.read())]
         coords = {(x1, y1): (x2, y2) for x1, y1, x2, y2 in coords}
 
-    print("Part 1:", part_1(coords, 10))
+    print("Part 1:", part_1(coords, 2000000))
     print("Part 2:", part_2(coords))
 
 
@@ -21,12 +21,13 @@ def part_1(coords: dict[tuple[int, int], tuple[int, int]], y_to_check: int) -> i
 
     for sensor, beacon in coords.items():
         distance = manhattan_distance(sensor, beacon)
-        for x in range(-distance, distance + 1):
+        sensor_x, _ = sensor
+        for x in range(-distance + sensor_x, distance + sensor_x + 1):
             pos = (x, y_to_check)
             if manhattan_distance(pos, sensor) <= distance:
-                beacon_no_go_zone.add(pos)
+                beacon_no_go_zone.add(pos[0])
 
-    return beacon_no_go_zone
+    return len(beacon_no_go_zone) - 1
 
 
 def manhattan_distance(start: tuple[int, int], end: tuple[int, int]) -> int:
