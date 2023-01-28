dbd10f03aa298e022bd298ed90552ebb053dc492
Michael Otty <michael.otty@gmail.com>
2023-01-29 02:33:35 +1100

    Add WIP for 2022 day 23

--- aoc2022/23/aoc202223.py
+++ aoc2022/23/aoc202223.py
@@ -1,23 +1,56 @@
 """Advent of code Day 23."""
 
+import io
+import operator
+
 
 def main():
     """Main function."""
     with open("aoc2022/23/test1.txt", encoding="utf-8") as file:
-        grid = [
-            [1 if char == "#" else 0 for char in line]
-            for line in file.read().splitlines()
-        ]
-    print("Part 1:", part_1(grid), sep="\n")
+        grid = Grid(file)
+    print(grid, "\n")
+    print("Part 1:", part_1(grid))
     print("Part 2:", part_2(grid))
 
 
-def part_1(grid):
+class Grid:
+    """Grid class."""
+
+    def __init__(self, file: io.TextIOWrapper) -> None:
+        """Create a Grid from text file."""
+        self.grid = {
+            (x, y)
+            for y, line in enumerate(file.read().splitlines())
+            for x, char in enumerate(line)
+            if char == "#"
+        }
+
+    def __repr__(self) -> str:
+        """Grid as string."""
+        min_x = min(self.grid, key=operator.index(0))
+        max_x = max(self.grid, key=operator.index(0))
+        min_y = min(self.grid, key=operator.index(1))
+        max_y = max(self.grid, key=operator.index(1))
+
+        lines = ("".join("#" if item else "." for item in line) for line in self.grid)
+        return "\n".join(lines)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self
+
+
+def part_1(grid: Grid):
     """Solve part 1."""
-    return "\n".join(str(line) for line in grid)
+    for i in range(10):
+        next(grid)
+        print(grid, "\n")
+    return 0
 
 
-def part_2(grid):
+def part_2(grid: Grid):
     """Solve part 2."""
     return 0
 
