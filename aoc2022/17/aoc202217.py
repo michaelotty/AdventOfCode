ea1876fdc84010f37cdfa2b2920694edf4bd6ba3
Michael Otty <michael.otty@gmail.com>
2023-03-30 22:38:19 +0200

    Solve part 1

--- aoc2022/17/aoc202217.py
+++ aoc2022/17/aoc202217.py
@@ -59,7 +59,7 @@ class LShape(Shape):
     @property
     def coords(self) -> set[tuple[int, int]]:
         """Get the starting coords for a shape."""
-        return {(0, x) for x in range(3)} | {(x, 0) for x in range(3)}
+        return {(0, x) for x in range(3)} | {(x, 2) for x in range(3)}
 
 
 class IShape(Shape):
@@ -91,52 +91,48 @@ class BoxShape(Shape):
 class Grid:
     """A grid."""
 
-    def __init__(self) -> None:
+    def __init__(self, file_name) -> None:
         """Create a grid."""
         self.grid = set()
+
+        with open(file_name, encoding="utf-8") as file:
+            self.directions_string = file.read()
 
         self.direction_generator = self._direction_generator()
         self.shape_generator = self._shape_generator()
 
     def _direction_generator(self):
         """Generate the infinite looping directions from the file."""
-        with open("aoc2022/17/test1.txt", encoding="utf-8") as file:
-            directions = file.read()
-
-        for char in itertools.cycle(directions):
+        for i, char in itertools.cycle(enumerate(self.directions_string)):
+            # print(i, char)
             yield char
 
     def _shape_generator(self):
         """Generate the shape to use infinitely."""
-        shapes = (MinusShape, PlusShape, LShape, IShape, BoxShape)[4:]
+        shapes = (MinusShape, PlusShape, LShape, IShape, BoxShape)
 
         for shape in itertools.cycle(shapes):
             yield shape()
 
-    def __next__(self):
-        """Add a shape to the grid, then iterate the movement until it stops."""
-        shape = next(self.shape_generator)
+    def get_max_row(self, grid):
+        if not grid:
+            grid = self.grid
 
-        height = 0
+        return max(row for row, _ in grid)
 
-        if self.grid:
-            height = self._get_max_row()
+    def _move_shape(self, shape, direction):
+        direction_mapping = {"right": (0, 1), "left": (0, -1), "down": (-1, 0)}
+        direction = direction_mapping[direction]
+        return {(row + direction[0], col + direction[1]) for row, col in shape}
 
-        active_shape = {(row + height + 3, col + 2) for row, col in shape.coords}
-
-        self.grid |= active_shape
-
-    def _get_max_row(self):
-        return max(row for row, _ in self.grid)
-
-    def __repr__(self) -> str:
-        """String representation of grid."""
+    def _str_of_grid(self, grid) -> str:
+        """String representation of a grid."""
         rows_to_print = []
 
-        for row in range(self._get_max_row() + 1):
+        for row in range(self.get_max_row(grid) + 1):
             row_line = []
             for col in range(7):
-                if (row, col) in self.grid:
+                if (row, col) in grid:
                     row_line.append("#")
                 else:
                     row_line.append(".")
@@ -147,21 +143,66 @@ class Grid:
 
         return "\n".join(rows_to_print)
 
+    def __next__(self):
+        """Add a shape to the grid, then iterate the movement until it stops."""
+        shape = next(self.shape_generator)
 
-def print_grid(grid):
-    """Print the grid representation."""
-    grid = zip(*grid)
-    print("\n".join("".join(line) for line in grid))
+        height = 0
+
+        if self.grid:
+            height = self.get_max_row(self.grid) + 1
+
+        active_shape = {(row + height + 3, col + 2) for row, col in shape.coords}
+
+        next_active_shape = active_shape.copy()
+
+        direction_mapping = {">": "right", "<": "left"}
+
+        falling = True
+
+        while falling:
+            next_active_shape = self._move_shape(
+                next_active_shape, direction_mapping[next(self.direction_generator)]
+            )
+
+            # print(self._str_of_grid(next_active_shape))
+            # print()
+
+            for row, col in next_active_shape:
+                if not 0 <= col < 7:
+                    next_active_shape = active_shape.copy()
+                    break
+
+                if (row, col) in self.grid:
+                    next_active_shape = active_shape.copy()
+                    break
+
+            active_shape = next_active_shape.copy()
+
+            next_active_shape = self._move_shape(next_active_shape, "down")
+
+            for row, col in next_active_shape:
+                if (row, col) in self.grid or row < 0:
+                    next_active_shape = active_shape.copy()
+                    falling = False
+                    break
+
+            active_shape = next_active_shape.copy()
+
+        self.grid |= active_shape
+
+    def __repr__(self) -> str:
+        """String representation of grid."""
+        return self._str_of_grid(self.grid)
 
 
 def part_1():
     """Solve part 1."""
-    grid = Grid()
-    next(grid)
+    grid = Grid("aoc2022/17/input.txt")
+    for _ in range(2022):
+        next(grid)
 
-    print(grid)
-
-    return 0
+    return grid.get_max_row(grid.grid) + 1
 
 
 def part_2():
