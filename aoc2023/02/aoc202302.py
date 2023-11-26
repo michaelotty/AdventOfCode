a0f22af899d2cdd1d67b7da46764f514e3067960
Michael Otty <michael.otty@gmail.com>
2023-11-26 12:58:08 +0000

    Add template for each day of 2023

---
+++ aoc2023/02/aoc202302.py
@@ -0,0 +1,24 @@
+"""Advent of code day 2."""
+
+
+def main():
+    """Main function."""
+    with open("aoc2022/02/input.txt", encoding="utf-8") as file:
+        data = file.read()
+
+    print(data)
+
+    print("Part 1:", part_1())
+    print("Part 2:", part_2())
+
+
+def part_1():
+    """Solve part 1."""
+
+
+def part_2():
+    """Solve part 2."""
+
+
+if __name__ == "__main__":
+    main()
