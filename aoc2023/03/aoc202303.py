b2fdd4bc713f038dcb49dd4e54f662300ddf6ca3
Michael Otty <michael.otty@gmail.com>
2023-12-03 09:52:15 +0000

    Solves test input but not real input

--- aoc2023/03/aoc202303.py
+++ aoc2023/03/aoc202303.py
@@ -1,23 +1,58 @@
 """Advent of code day 3."""
 
+import re
+from itertools import product
+
 
 def main():
     """Main function."""
     with open("aoc2023/03/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        rows = file.read().splitlines()
 
-    print(data)
-
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    print("Part 1:", part_1(rows))
+    # print("Part 2:", part_2(rows))
 
 
-def part_1():
+def part_1(rows: list[str]):
     """Solve part 1."""
+    symbol_coords = set()
+    height = len(rows)
+    width = len(rows[0])
+
+    for i, row in enumerate(rows):
+        for j, char in enumerate(row):
+            if re.match(r"[^\.\d]", char):
+                symbol_coords |= {
+                    (i + x, j + y)
+                    for x, y in product(range(-1, 2), range(-1, 2))
+                    if 0 <= i + x < height and 0 <= j + y < width
+                }
+
+    part_numbers = []
+
+    for i, row in enumerate(rows):
+        number = []
+        is_part_number = False
+
+        for j, char in enumerate(row):
+            if re.match(r"\d", char):
+                number.append(char)
+                if (i, j) in symbol_coords:
+                    is_part_number = True
+            elif number and is_part_number:
+                part_numbers.append(int("".join(number)))
+                number = []
+                is_part_number = False
+
+        if number and is_part_number:
+            part_numbers.append(int("".join(number)))
+
+    return sum(part_numbers)  # 258681945906 too high
 
 
-def part_2():
+def part_2(rows):
     """Solve part 2."""
+    return rows
 
 
 if __name__ == "__main__":
