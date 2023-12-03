752cd7c6b124c8f1ead8cee74034b19a2d7cbd90
Michael Otty <michael.otty@gmail.com>
2023-12-03 12:59:29 +0000

    Fix black linter error

--- 2021/src/day_19.py
+++ 2021/src/day_19.py
@@ -9,38 +9,18 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def day_19(scanners):
-    """Solve day 19"""
-    best_angles = None
-    best_distance = None
-    best_sensitivity = 1.0
-
-    for sub_set_a in permutations(scanners[0], 12):
-        for sub_set_b in permutations(scanners[1], 12):
-            angles, distance, sensitivity = R.align_vectors(
-                sub_set_a, sub_set_b, return_sensitivity=True)
-            if distance < 500**2:
-                best_sensitivity = sensitivity.mean()
-                best_angles = angles
-                best_distance = distance
-                print(best_angles.as_euler('XYZ', degrees=True), distance, sep='\t')
-
-    best_angles = best_angles.as_euler('XYZ', degrees=True)
-    print(best_angles)
-    print(best_distance)
-
-
-if __name__ == "__main__":
-    FILE_NAME = "test.txt"
+def main():
+    """Program starts here."""
     scanners = []
     i = -1
 
-    with open("data/19/" + FILE_NAME, encoding='utf-8') as file:
+    with open("2021/data/19/test.txt", encoding="utf-8") as file:
         for line in file:
-            split_line = re.findall(r'-*\d+', line)
+            split_line = re.findall(r"-*\d+", line)
             if len(split_line) == 3:
                 scanners[i] = np.append(
-                    scanners[i], [float(item) for item in split_line])
+                    scanners[i], [float(item) for item in split_line]
+                )
             elif len(split_line) == 1:
                 scanners.append(np.array([]))
                 i += 1
@@ -49,3 +29,29 @@ if __name__ == "__main__":
         scanners[i] = np.reshape(scanner, (len(scanner) // 3, 3))
 
     day_19(scanners)
+
+
+def day_19(scanners):
+    """Solve day 19"""
+    best_angles = None
+    best_distance = None
+    # best_sensitivity = 1.0
+
+    for sub_set_a in permutations(scanners[0], 12):
+        for sub_set_b in permutations(scanners[1], 12):
+            angles, distance, _ = R.align_vectors(
+                sub_set_a, sub_set_b, return_sensitivity=True
+            )
+            if distance < 500**2:
+                # best_sensitivity = sensitivity.mean()
+                best_angles = angles
+                best_distance = distance
+                print(best_angles.as_euler("XYZ", degrees=True), distance, sep="\t")
+
+    best_angles = best_angles.as_euler("XYZ", degrees=True)
+    print(best_angles)
+    print(best_distance)
+
+
+if __name__ == "__main__":
+    main()
