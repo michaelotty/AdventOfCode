0ecd2dae02a06189f44d2c4700bfc1cf7cfb9c06
Michael Otty <michael.otty@gmail.com>
2023-12-09 13:27:41 +0000

    Solve part 1 day 9

--- 2023/09/aoc202309.py
+++ 2023/09/aoc202309.py
@@ -4,16 +4,28 @@
 def main():
     """Main function."""
     with open("2023/09/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        histories = [
+            [int(num) for num in line.split()] for line in file.read().splitlines()
+        ]
 
-    print(data)
-
-    print("Part 1:", part_1())
+    print("Part 1:", part_1(histories))
     print("Part 2:", part_2())
 
 
-def part_1():
+def part_1(histories: list[list[int]]) -> int:
     """Solve part 1."""
+    values = []
+
+    for history in histories:
+        analyse = [history]
+        while not all(item == 0 for item in analyse[-1]):
+            new_item = [
+                right - left for left, right in zip(analyse[-1][:-1], analyse[-1][1:])
+            ]
+            analyse.append(new_item)
+
+        values.append(sum(line[-1] for line in analyse))
+    return sum(values)
 
 
 def part_2():
