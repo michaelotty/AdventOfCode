af15b803aada971dbb5aa93646f51ef4e9feb1a5
Michael Otty <michael.otty@gmail.com>
2023-12-15 17:17:28 +0000

    Change data structure

--- 2023/05/aoc202305.py
+++ 2023/05/aoc202305.py
@@ -9,19 +9,28 @@ class Day5Solver:
         seeds_text, *rest_text = input_text.split("\n\n")
         self._seeds = [int(x) for x in seeds_text.split()[1:]]
 
-        self.mapping: dict[tuple[str, str], list[tuple[int, ...]]] = {}
+        self.mapping: dict[tuple[str, str], list[dict[str, int]]] = {}
 
         for block in rest_text:
             title, *number_lines = block.splitlines()
             from_location, to_location = title.split()[0].split("-to-")
-            self.mapping[(from_location, to_location)] = [
-                tuple(map(int, x.split())) for x in number_lines
-            ]
+
+            self.mapping[(from_location, to_location)] = []
+
+            for number_line in number_lines:
+                dest_range, source_range, range_len = number_line.split()
+                self.mapping[(from_location, to_location)].append(
+                    {
+                        "dest_range": int(dest_range),
+                        "source_range": int(source_range),
+                        "range_len": int(range_len),
+                    }
+                )
 
     @property
     def seeds(self):
         """Property of seed IDs."""
-        return self._seeds
+        return self._seeds.copy()
 
     def __repr__(self) -> str:
         """Return a string representation."""
@@ -29,11 +38,17 @@ class Day5Solver:
 
         mapping_str = ""
         for key, val in self.mapping.items():
-            mapping_str += "\n\n"
-            mapping_str += key[0] + "-to-" + key[1] + " map:\n"
-            mapping_str += "\n".join(map(str, val))
+            mapping_str += f"\n\n{key[0]}-to-{key[1]} map:\n"
+            mapping_str += "\n".join(
+                f'{line["dest_range"]} {line["source_range"]} {line["range_len"]}'
+                for line in val
+            )
 
         return f"seeds: {seeds_str}{mapping_str}"
+
+    def solve(self) -> int:
+        """Solve the solution."""
+        return 0
 
 
 def main() -> None:
@@ -44,13 +59,13 @@ def main() -> None:
     solver = Day5Solver(file_text)
     print(solver)
 
-    print("Part 1:", part_1())
+    print("Part 1:", part_1(solver))
     print("Part 2:", part_2())
 
 
-def part_1() -> int:
+def part_1(solver: Day5Solver) -> int:
     """Solve part 1."""
-    return 0
+    return solver.solve()
 
 
 def part_2() -> int:
