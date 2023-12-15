235e57d3a915db489d589bf78c7ff15aa57dc597
Michael Otty <michael.otty@gmail.com>
2023-12-15 18:35:17 +0000

    Read input to lists

--- 2023/06/aoc202306.py
+++ 2023/06/aoc202306.py
@@ -4,16 +4,24 @@
 def main() -> None:
     """Program starts here."""
     with open("2023/06/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        text = file.read()
 
-    print(data)
+    first_line, second_line = text.splitlines()
 
-    print("Part 1:", part_1())
+    _, *time_strs = first_line.split()
+    _, *distance_strs = second_line.split()
+
+    times = [int(x) for x in time_strs]
+    distances = [int(x) for x in distance_strs]
+
+    print("Part 1:", part_1(times, distances))
     print("Part 2:", part_2())
 
 
-def part_1() -> int:
+def part_1(times: list[int], distances: list[int]) -> int:
     """Solve part 1."""
+    print(times)
+    print(distances)
     return 0
 
 
