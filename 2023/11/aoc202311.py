25ae0172c1550bf0922c2a8a4630c56c153f9396
Michael Otty <michael.otty@gmail.com>
2023-12-16 13:52:34 +0000

    Upgrade for a more efficient method for part 2

--- 2023/11/aoc202311.py
+++ 2023/11/aoc202311.py
@@ -8,33 +8,44 @@ class GalaxyMap:
 
     def __init__(self, text: str) -> None:
         """Create a galaxy map."""
-        self.grid = [list(line) for line in text.splitlines()]
+        self.galaxy_coords = set()
+        grid = [list(line) for line in text.splitlines()]
+        self.width = len(grid[0])
+        self.height = len(grid)
 
-    def expand_universe(self):
-        """Expand the universe."""
-        empty_rows = [len(set(line)) == 1 for line in self.grid]
-        empty_cols = [len(set(col)) == 1 for col in zip(*self.grid)]
-
-        empty_row = ["." for _ in self.grid[0]]
-
-        for i, is_empty in reversed(list(enumerate(empty_rows))):
-            if is_empty:
-                self.grid.insert(i, empty_row.copy())
-
-        for i, is_empty in reversed(list(enumerate(empty_cols))):
-            if is_empty:
-                for row in self.grid:
-                    row.insert(i, ".")
-
-    @property
-    def galaxy_coords(self) -> set[tuple[int, int]]:
-        """Get the galaxy coordinates."""
-        galaxy_coords = set()
-        for i, row in enumerate(self.grid):
+        for i, row in enumerate(grid):
             for j, char in enumerate(row):
                 if char == "#":
-                    galaxy_coords.add((i, j))
-        return galaxy_coords
+                    self.galaxy_coords.add((i, j))
+
+    def expand_universe(self, amount: int = 2) -> None:
+        """Expand the universe."""
+        amount -= 1
+        # Expand the rows first
+        for i in reversed(range(self.height)):
+            # Handle not empty row early
+            if any((i, j) in self.galaxy_coords for j in range(self.width)):
+                continue
+
+            self.height += amount
+
+            for coord in self.galaxy_coords.copy():
+                if coord[0] > i:
+                    self.galaxy_coords.remove(coord)
+                    self.galaxy_coords.add((coord[0] + amount, coord[1]))
+
+        # Then columns
+        for j in reversed(range(self.width)):
+            # Handle not empty col early
+            if any((i, j) in self.galaxy_coords for i in range(self.height)):
+                continue
+
+            self.width += amount
+
+            for coord in self.galaxy_coords.copy():
+                if coord[1] > j:
+                    self.galaxy_coords.remove(coord)
+                    self.galaxy_coords.add((coord[0], coord[1] + amount))
 
     @property
     def galaxy_routes(self):
@@ -51,7 +62,10 @@ class GalaxyMap:
 
     def __repr__(self) -> str:
         """Return a string representation."""
-        return "\n".join("".join(line) for line in self.grid)
+        grid = [["." for _ in range(self.width)] for _ in range(self.height)]
+        for coord in self.galaxy_coords:
+            grid[coord[0]][coord[1]] = "#"
+        return "\n".join("".join(line) for line in grid)
 
 
 def manhattan_distance(from_coord: tuple[int, int], to_coord: tuple[int, int]) -> int:
@@ -64,21 +78,20 @@ def main() -> None:
     with open("2023/11/input.txt", encoding="utf-8") as file:
         text = file.read()
 
-    data = GalaxyMap(text)
-
-    print("Part 1:", part_1(data))
-    print("Part 2:", part_2())
+    print("Part 1:", part_1(GalaxyMap(text)))
+    print("Part 2:", part_2(GalaxyMap(text)))
 
 
-def part_1(data) -> int:
+def part_1(galaxy_map) -> int:
     """Solve part 1."""
-    data.expand_universe()
-    return data.solve()
+    galaxy_map.expand_universe()
+    return galaxy_map.solve()
 
 
-def part_2() -> int:
+def part_2(galaxy_map) -> int:
     """Solve part 2."""
-    return 0
+    galaxy_map.expand_universe(1000000)
+    return galaxy_map.solve()
 
 
 if __name__ == "__main__":
