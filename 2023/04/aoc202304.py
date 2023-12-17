8fbf105fd0e885b7e5fba3abc0f79b0ab3b2204f
Michael Otty <michael.otty@gmail.com>
2023-12-17 19:46:56 +0000

    Read in data

--- 2023/04/aoc202304.py
+++ 2023/04/aoc202304.py
@@ -1,12 +1,25 @@
 """Advent of code day 4."""
 
+import re
+
 
 def main() -> None:
     """Program starts here."""
     with open("2023/04/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        raw_data = [
+            re.split(r"(?:: )|(?: \| )", line)[1:] for line in file.read().splitlines()
+        ]
 
-    print(data)
+    data = [
+        (
+            [int(num) for num in re.findall(r"\d+", left_str)],
+            [int(num) for num in re.findall(r"\d+", right_str)],
+        )
+        for left_str, right_str in raw_data
+    ]
+
+    for line in data:
+        print(line)
 
     print("Part 1:", part_1())
     print("Part 2:", part_2())
