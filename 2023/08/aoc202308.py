9176dbe3f541121b280b720b6669e62b088a2309
Michael Otty <michael.otty@gmail.com>
2023-12-17 18:38:17 +0000

    Solve part 1

--- 2023/08/aoc202308.py
+++ 2023/08/aoc202308.py
@@ -1,19 +1,45 @@
 """Advent of code day 8."""
 
+import itertools
+import re
+
 
 def main() -> None:
     """Program starts here."""
     with open("2023/08/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        steps, mapping_text = file.read().split("\n\n")
 
-    print(data)
+    mapping = create_mapping(mapping_text)
 
-    print("Part 1:", part_1())
+    print("Part 1:", part_1(steps, mapping))
     print("Part 2:", part_2())
 
 
-def part_1() -> int:
+def create_mapping(mapping_text: str) -> dict[str, tuple[str, str]]:
+    """Create a mapping from the text input."""
+    mapping = {}
+
+    for line in mapping_text.splitlines():
+        key, left, right = re.findall(r"\w+", line)
+        mapping[key] = (left, right)
+
+    return mapping
+
+
+def part_1(steps, mapping) -> int:
     """Solve part 1."""
+    position = "AAA"
+    distance_travelled = 0
+    for direction in itertools.cycle(steps):
+        distance_travelled += 1
+        if direction == "L":
+            position = mapping[position][0]
+        else:
+            position = mapping[position][1]
+
+        if position == "ZZZ":
+            return distance_travelled
+
     return 0
 
 
