9026448ae98f407f1736c8b50f409f5c9be6af56
Michael Otty <michael.otty@gmail.com>
2023-12-20 15:54:12 +0000

    Add some regex stuff

--- 2023/12/aoc202312.py
+++ 2023/12/aoc202312.py
@@ -1,12 +1,15 @@
 """Advent of code day 12."""
 
+import re
+
 
 def main() -> None:
     """Program starts here."""
-    with open("2023/12/input.txt", encoding="utf-8") as file:
-        data = file.read()
+    with open("2023/12/test.txt", encoding="utf-8") as file:
+        lines = [line.split() for line in file.read().splitlines()]
 
-    print(data)
+    for line in lines:
+        print(re.findall(r"(?:\?|#)+", line[0]), line[1])
 
     print("Part 1:", part_1())
     print("Part 2:", part_2())
