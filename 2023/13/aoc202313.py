0f7e73d97cd2b9bbf398feca96f04086a2436dbd
Michael Otty <michael.otty@gmail.com>
2024-01-14 14:43:46 +0000

    Fix and solve off by 1 error in part 1

--- 2023/13/aoc202313.py
+++ 2023/13/aoc202313.py
@@ -1,5 +1,7 @@
 """Advent of code day 13."""
 
+import logging
+
 
 def main() -> None:
     """Program starts here."""
@@ -12,7 +14,7 @@ def main() -> None:
 
 def part_1(patterns: list[list[str]]) -> int:
     """Solve part 1."""
-    return sum(find_reflection_score(pattern) for pattern in patterns)  # 27567 too high
+    return sum(find_reflection_score(pattern) for pattern in patterns)
 
 
 def part_2() -> int:
@@ -23,18 +25,20 @@ def part_2() -> int:
 def find_reflection_score(pattern: list[str]) -> int:
     """Find the score for a pattern."""
     # Check rows first
-    for i in range(len(pattern) - 2):
-        if all(up == down for up, down in zip(pattern[i::-1], pattern[i + 1 : -1])):
-            print(f"Reflection between row {i+1} and {i+2}")
+    for i in range(len(pattern) - 1):
+        if all(up == down for up, down in zip(pattern[i::-1], pattern[i + 1 :])):
+            logging.debug("Reflection between row %d and %d", i + 1, i + 2)
             return 100 * (i + 1)
 
     cols = ["".join(x) for x in zip(*pattern)]
 
     # Then columns
-    for i in range(len(cols) - 2):
+    for i in range(len(cols) - 1):
         if all(left == right for left, right in zip(cols[i::-1], cols[i + 1 : -1])):
-            print(f"Reflection between col {i+1} and {i+2}")
+            logging.debug("Reflection between col %d and %d", i + 1, i + 2)
             return i + 1
+
+    logging.debug("No reflections found")
     return 0
 
 
