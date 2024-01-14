3ac5c5c4b10f051f6fb7c7e109964c18da0e3930
Michael Otty <michael.otty@gmail.com>
2024-01-14 17:15:07 +0000

    Solution to part 2 not quite right

--- 2023/13/aoc202313.py
+++ 2023/13/aoc202313.py
@@ -1,6 +1,7 @@
 """Advent of code day 13."""
 
 import logging
+from collections.abc import Iterator
 
 
 def main() -> None:
@@ -9,7 +10,7 @@ def main() -> None:
         patterns = [pattern.splitlines() for pattern in file.read().split("\n\n")]
 
     print("Part 1:", part_1(patterns))
-    print("Part 2:", part_2())
+    print("Part 2:", part_2(patterns))
 
 
 def part_1(patterns: list[list[str]]) -> int:
@@ -17,9 +18,11 @@ def part_1(patterns: list[list[str]]) -> int:
     return sum(find_reflection_score(pattern) for pattern in patterns)
 
 
-def part_2() -> int:
+def part_2(patterns: list[list[str]]) -> int:
     """Solve part 2."""
-    return 0
+    return sum(
+        find_smudged_reflection_score(pattern) for pattern in patterns
+    )  # 41350 too low, 41494 too low
 
 
 def find_reflection_score(pattern: list[str]) -> int:
@@ -42,5 +45,36 @@ def find_reflection_score(pattern: list[str]) -> int:
     return 0
 
 
+def find_smudged_reflection_score(pattern: list[str]) -> int:
+    """Find the score for a pattern that has a smudge."""
+    # Check rows first
+    for i in range(len(pattern) - 1):
+        if calculate_difference(zip(pattern[i::-1], pattern[i + 1 :])) == 1:
+            logging.debug("Reflection between row %d and %d", i + 1, i + 2)
+            return 100 * (i + 1)
+
+    cols = ["".join(x) for x in zip(*pattern)]
+
+    # Then columns
+    for i in range(len(cols) - 1):
+        if calculate_difference(zip(cols[i::-1], cols[i + 1 : -1])) == 1:
+            logging.debug("Reflection between col %d and %d", i + 1, i + 2)
+            return i + 1
+
+    logging.debug("No reflections found")
+    return 0
+
+
+def calculate_difference(rows: Iterator[tuple[str, str]]) -> int:
+    """Calculate the number of character differences between rows."""
+    differences = 0
+    for left, right in rows:
+        for left_char, right_char in zip(left, right):
+            if left_char != right_char:
+                differences += 1
+    return differences
+
+
 if __name__ == "__main__":
+    logging.basicConfig(filename="output.log", filemode="w", level=logging.DEBUG)
     main()
