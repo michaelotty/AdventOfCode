77282a31d58aa5cade44bfa127b1bc99c7d73ce1
Michael Otty <michael.otty@gmail.com>
2024-01-21 13:10:59 +0000

    Format in another way

--- 2021/src/21.cpp
+++ 2021/src/21.cpp
@@ -256,9 +256,7 @@ public:
     RealGame(std::pair<int64_t, int64_t> startingPositions)
         : m_player1Position(startingPositions.first),
           m_player2Position(startingPositions.second),
-          m_scores(),
-          m_answer{0, 0},
-          m_foundAnswer(false)
+          m_scores(), m_answer{0, 0}, m_foundAnswer(false)
     {
     }
 
