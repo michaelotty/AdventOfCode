cdff0010061218332e2a875ed548394dd54df2c8
Michael Otty <michael.otty@gmail.com>
2024-02-04 13:41:53 +0000

    Organise input data

--- 2023/17/aoc202317.py
+++ 2023/17/aoc202317.py
@@ -3,23 +3,21 @@
 
 def main() -> None:
     """Program starts here."""
-    with open("2023/17/input.txt", encoding="utf-8") as file:
-        data = file.read()
+    with open("2023/17/test.txt", encoding="utf-8") as file:
+        grid = [list(map(int, line)) for line in file.read().splitlines()]
 
-    print(data)
-
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    print("Part 1:", part_1(grid))
+    print("Part 2:", part_2(grid))
 
 
-def part_1() -> int:
+def part_1(grid: list[list[int]]) -> int:
     """Solve part 1."""
-    return 0
+    return grid[0][0]
 
 
-def part_2() -> int:
+def part_2(grid: list[list[int]]) -> int:
     """Solve part 2."""
-    return 0
+    return grid[0][0]
 
 
 if __name__ == "__main__":
