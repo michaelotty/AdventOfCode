65eeccc7903f02fcbfe40e344d6266485acd364f
Michael Otty <michael.otty@gmail.com>
2024-12-12 19:50:36 +0000

    Solve part 1

--- 2024/03/aoc202403.py
+++ 2024/03/aoc202403.py
@@ -1,23 +1,24 @@
 """Advent of code day 3."""
 
+import re
+
 
 def main() -> None:
     """Program starts here."""
     with open("2024/03/input.txt", encoding="utf-8") as file:
         data = file.read()
 
-    print(data)
-
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
 
 
-def part_1() -> int:
+def part_1(data) -> int:
     """Solve part 1."""
-    return 0
+    data = [(int(x), int(y)) for x, y in re.findall(r"mul\((\d+),(\d+)\)", data)]
+    return sum(x * y for x, y in data)
 
 
-def part_2() -> int:
+def part_2(data) -> int:
     """Solve part 2."""
     return 0
 
