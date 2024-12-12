33bd14ca85a2e569f0222e5dc63d545a034bf93c
Michael Otty <michael.otty@gmail.com>
2024-12-12 19:15:50 +0000

    Solve part 2

--- 2024/01/aoc202401.py
+++ 2024/01/aoc202401.py
@@ -7,7 +7,7 @@ def main() -> None:
         data = [tuple(map(int, x.split())) for x in file.read().splitlines()]
 
     print("Part 1:", part_1(data))
-    print("Part 2:", part_2())
+    print("Part 2:", part_2(data))
 
 
 def part_1(data) -> int:
@@ -21,9 +21,18 @@ def part_1(data) -> int:
     return sum(sums)
 
 
-def part_2() -> int:
+def part_2(data) -> int:
     """Solve part 2."""
-    return 0
+    data = list(zip(*data))
+    total = 0
+    for i in data[0]:
+        count = 0
+        for j in data[1]:
+            if i == j:
+                count += 1
+        total += i * count
+
+    return total
 
 
 if __name__ == "__main__":
