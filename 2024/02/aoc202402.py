0e39bd0c606a3a4ff04ff3aac9dccffe35adb4ee
Michael Otty <michael.otty@gmail.com>
2024-12-12 19:26:42 +0000

    Solve part 1

--- 2024/02/aoc202402.py
+++ 2024/02/aoc202402.py
@@ -4,20 +4,28 @@
 def main() -> None:
     """Program starts here."""
     with open("2024/02/input.txt", encoding="utf-8") as file:
-        data = file.read()
+        data = [[int(x) for x in line.split()] for line in file]
 
-    print(data)
-
-    print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
 
 
-def part_1() -> int:
+def part_1(data) -> int:
     """Solve part 1."""
-    return 0
+    return sum(all_increasing(line) or all_decreasing(line) for line in data)
 
 
-def part_2() -> int:
+def all_increasing(line) -> bool:
+    """Check all numbers increase."""
+    return all(1 <= (y - x) <= 3 for x, y in zip(line[:-1], line[1:]))
+
+
+def all_decreasing(line) -> bool:
+    """Check all numbers decrease."""
+    return all(-3 <= (y - x) <= -1 for x, y in zip(line[:-1], line[1:]))
+
+
+def part_2(data) -> int:
     """Solve part 2."""
     return 0
 
