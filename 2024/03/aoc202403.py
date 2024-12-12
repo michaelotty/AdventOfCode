109b97873573eeaf6a5361a26ab41f42df687557
Michael Otty <michael.otty@gmail.com>
2024-12-12 20:00:11 +0000

    Solve part 2

--- 2024/03/aoc202403.py
+++ 2024/03/aoc202403.py
@@ -18,9 +18,19 @@ def part_1(data) -> int:
     return sum(x * y for x, y in data)
 
 
-def part_2(data) -> int:
+def part_2(data: str) -> int:
     """Solve part 2."""
-    return 0
+    proc_data = data.split("don't()")
+    data_to_join = [proc_data[0]]  # First block is always valid
+
+    for item in proc_data[1:]:
+        split_bad_str = item.split("do()", maxsplit=1)
+        if len(split_bad_str) == 1:
+            continue
+        _, good = split_bad_str
+        data_to_join.append(good)
+
+    return part_1("".join(data_to_join))
 
 
 if __name__ == "__main__":
